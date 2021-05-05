888a2d676cdd34ae046019a48e2373cdd424a26b
GGunt3r <65355978+GGunt3r@users.noreply.github.com>
2021-05-05 15:51:45 -0300

    Organizando

--- index.js
+++ index.js
@@ -4,20 +4,11 @@ var http = require("http").createServer(app);
 var io = require("socket.io")(http);
 
 io.on('connection', (socket) => {
-
     socket.on('disconnect', () => {
         console.log('Usuário desconectado, ID: ' + socket.id);
     });
 
-    socket.on('client', (data) => {
-        console.log('EVENTO DISPARADO')
-        console.log(data);
-    });
-
-    socket.on('palavra', (data) => {
-        console.log(data);
-        socket.emit('resultado', data + ' - Mensagem enviada pelo input');
-    });
+    
 });
 
 app.set('view engine', 'ejs');
